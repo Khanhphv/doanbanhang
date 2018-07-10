f0c4d53694b2b31c705690a4e52e15698ea4d36d
tungthanhnp <tungthanhnp@gmail.com>
2018-07-10 20:46:43 +0700

    lan2

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,8 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/ddsfjkfjfhkjhfkjhkjfd
-sdsd
+*/
+edit lan2
 Route::get('/', function () {
     return view('welcome');
 });
